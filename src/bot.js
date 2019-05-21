73b71906471c1fa9b0f00ab74957b913d252a7f9
Anton Vdovichenko <ant-vav@ya.ru>
2019-05-21 21:47:23 +0300

    New example

--- src/bot.js
+++ src/bot.js
@@ -4,13 +4,14 @@ const Telegraf = require('telegraf');
 
 const bot = new Telegraf(process.env.TOKEN);
 
-bot.command('oldschool', (ctx) => ctx.reply('Hello'));
-bot.command('modern', ({reply}) => reply('Yo'));
-bot.command('hipster', Telegraf.reply('λ'));
-bot.launch();
-
-// bot.start((ctx) => ctx.reply('Welcome!'));
-// bot.help((ctx) => ctx.reply('Send me a sticker'));
-// bot.on('sticker', (ctx) => ctx.reply('👍'));
-// bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+// bot.command('oldschool', (ctx) => ctx.reply('Hello'));
+// bot.command('modern', ({reply}) => reply('Yo'));
+// bot.command('hipster', Telegraf.reply('λ'));
 // bot.launch();
+
+bot.start((ctx) => ctx.reply('Welcome!'));
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.on('sticker', (ctx) => ctx.reply('👍'));
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.hears('help', (ctx) => ctx.reply());
+bot.launch();

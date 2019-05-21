ffa9d2da565c77d5e5fc76be29874ea624909be1
Anton Vdovichenko <ant-vav@ya.ru>
2019-05-21 20:44:02 +0300

    Initial commit

---
+++ src/bot.js
@@ -0,0 +1,16 @@
+'use strict';
+const Telegraf = require('telegraf');
+const config = require('../config/config');
+
+const bot = new Telegraf(process.env.BotToken);
+
+bot.command('oldschool', (ctx) => ctx.reply('Hello'));
+bot.command('modern', ({reply}) => reply('Yo'));
+bot.command('hipster', Telegraf.reply('λ'));
+bot.launch();
+
+// bot.start((ctx) => ctx.reply('Welcome!'));
+// bot.help((ctx) => ctx.reply('Send me a sticker'));
+// bot.on('sticker', (ctx) => ctx.reply('👍'));
+// bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+// bot.launch();

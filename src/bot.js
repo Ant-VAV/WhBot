3d458164616b696cb61536aca8cbbb4bc38bc0fd
Anton Vdovichenko <ant-vav@ya.ru>
2019-05-21 22:06:51 +0300

    Tips

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,6 @@
 'use strict';
 const Telegraf = require('telegraf');
+const tips = require('./tips.js').tips;
 // const config = require('../config/config');
 
 const bot = new Telegraf(process.env.TOKEN);
@@ -13,5 +14,5 @@ bot.start((ctx) => ctx.reply('Welcome!'));
 bot.help((ctx) => ctx.reply('Send me a sticker'));
 bot.on('sticker', (ctx) => ctx.reply('👍'));
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
-bot.hears('help', (ctx) => ctx.reply());
+bot.hears('help', (ctx) => ctx.reply(tips[Math.random() * tips.length]));
 bot.launch();
